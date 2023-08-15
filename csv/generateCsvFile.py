782505d675c7e3ba395f360bdffa7f2703c903d4
zahrabateninia <zahrabatenin@gmail.com>
2023-08-15 17:27:41 +0330

    generating a csv file with two rows

---
+++ csv/generateCsvFile.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import csv
+
+hosts = [["workstation.local", "192.168.25.46"], ["webserver.cloud", "10.2.5.6"]]
+with open("hosts.csv", "w") as hosts_csv:
+    writer = csv.writer(hosts_csv)
+    writer.writerows(hosts)
