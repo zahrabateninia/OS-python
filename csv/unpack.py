1d0a73fd275cc03931519f66ba1ee1eacbb5b5ba
zahrabateninia <zahrabatenin@gmail.com>
2023-08-15 17:15:57 +0330

    unpack the list into name variables

---
+++ csv/unpack.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3 
+import csv
+
+f = open("csv_file.txt")
+csv_f = csv.reader(f)
+for row in csv_f:
+    name, age, role = row
+    print("Name: {}, Age: {}, Role:{}".format(name, age, role))
