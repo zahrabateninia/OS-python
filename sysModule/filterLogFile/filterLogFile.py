c980927b9594fd51d8e662d5b2b5c675d9c80655
zahrabateninia <zahrabatenin@gmail.com>
2023-08-28 19:09:10 +0330

    making sense out of the Data

--- sysModule/filterLogFile/filterLogFile.py
+++ sysModule/filterLogFile/filterLogFile.py
@@ -3,10 +3,15 @@ import re
 import sys
 
 logfile = sys.argv[1]
+usernames = {}
 with open(logfile) as f:
     for line in f:
         if "CRON" not in line:
             continue
         pattern = r"USER \((\w+)\)$"
         result = re.search(pattern, line)
-        print(result[1])
+        if result is None:
+            continue
+        name = result[1]
+        usernames[name] = usernames.get(name, 0) + 1
+print(usernames)
