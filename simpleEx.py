984c1329f8db5f3519825577c99e4db149aba0c6
zahrabateninia <zahrabatenin@gmail.com>
2023-10-08 07:31:06 +0330

    checking cpu usage using psutil module

--- simpleEx.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-subprocess.run(["date"])
-subprocess.run(["sleep","2"]) 
-# The run function receives a list that starts with the name of the command
-#  that we wanna call followed by any parameters that we wanna pass to that command
--- simpleEx2.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-result = subprocess.run(["ls", "this_file_doe_not_exist"])
-print(result.returncode)
